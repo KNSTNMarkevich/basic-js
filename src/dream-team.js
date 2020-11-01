459e2ae64133f954aa6f4ab128457e1ed1735615
KNSTNMarkevich <konstantinmarkevich@gmail.com>
2020-11-01 20:20:28 +0300

    feat: complete tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) === true) {
+    let team = '';
+    let member;
+    let name;
+    for (index1 = 0; index1 < members.length; index1 += 1) {
+      member = members[index1];
+      if (typeof member === 'string') {
+        for (index2 = 0; index2 < member.length; index2 += 1) {
+        name = member[index2];
+        if (name !== ' ') {
+          team += name;
+          break;
+        };
+        }
+      }
+    }
+    return team.toUpperCase().split('').sort().join('');
+  } else {
+    return false;
+  }
 };
